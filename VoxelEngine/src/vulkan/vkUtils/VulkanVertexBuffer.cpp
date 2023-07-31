51513e0dbcfd26d38cd99f6667553dd8ec2a002f
Denis Kozarezov <52127090+DenisKozarezov@users.noreply.github.com>
2023-07-31 12:34:14 +0300

    Refactored buffers

--- VoxelEngine/src/vulkan/vkUtils/VulkanVertexBuffer.cpp
+++ VoxelEngine/src/vulkan/vkUtils/VulkanVertexBuffer.cpp
@@ -26,8 +26,7 @@ namespace vkUtils
 			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
 		vulkan::copyBuffer(stagingBuffer.buffer, vertexBuffer.buffer, bufferSize);
 
-		memory::destroyBuffer(logicalDevice, stagingBuffer.buffer);
-		memory::freeDeviceMemory(logicalDevice, stagingBuffer.bufferMemory);
+		stagingBuffer.release(logicalDevice);
 	}
 
 	void VulkanVertexBuffer::release() const
